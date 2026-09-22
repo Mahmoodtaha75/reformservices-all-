89337ff33eba4630a1f4d24d4ac1f7ffb2c407e9
Mahmoodtaha75 <152767736+Mahmoodtaha75@users.noreply.github.com>
2026-09-22 17:49:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ hideAllForms();
 // Add event listener to the call us now button
 const callUsNowBtn = document.getElementById('callUsNowBtn');
 callUsNowBtn.addEventListener('click', () => {
-    const phoneNumber = '+919886106311';
+    const phoneNumber = '+918892386209';
     const callLink = `tel:${phoneNumber}`;
     window.open(callLink, '_blank');
 });
